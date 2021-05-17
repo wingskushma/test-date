6b493d91fcb1debd866319d08905f48f6dccaaca
Kushma Thapa <kushma.thapa@cotiviti.com>
2021-05-17 18:27:01 +0545

    use local storage to store user enetered data, to show on refresh too

--- src/components/Output.vue
+++ src/components/Output.vue
@@ -11,9 +11,21 @@ export default {
   computed: {
     formattedDateList() {
       let formattedList = [];
+      //localStorage.clear();
       const enteredDate = this.$store.getters.getEnteredDate;
-      formattedList.push(new Date(enteredDate).toLocaleDateString("pl-PL"));
-      formattedList.push(new Date(enteredDate).toLocaleDateString("hu-HU"));
+      var old = localStorage.getItem("dataEnteredList");
+      console.log(old, " old data");
+      if (old === null) {
+        localStorage.setItem("dataEnteredList", enteredDate);
+      } else {
+        localStorage.setItem("dataEnteredList", old + "," + enteredDate);
+      }
+      var updatedData = localStorage.getItem("dataEnteredList").split(",");
+      console.log(updatedData, " new data");
+      for (const item of updatedData) {
+        formattedList.push(new Date(item).toLocaleDateString("pl-PL"));
+        formattedList.push(new Date(item).toLocaleDateString("hu-HU"));
+      }
       console.log(formattedList);
       return formattedList;
     },
